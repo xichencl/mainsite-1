42851da5381455b15357430e78a4fb0fe1651fca
howdycrystalm <howdycrystalm@gmail.com>
2017-08-05 09:19:57 -0700

    changed user's bubble to orange

--- client/assets/js/chat/Chatbox.jsx
+++ client/assets/js/chat/Chatbox.jsx
@@ -4,68 +4,66 @@ import { connect } from 'react-redux';
 import axios from 'axios';
 
 import ChatBubble from './ChatBubble.jsx';
-
+//trying to put bot's bubble on left, and user's bubble on right of chatbox
+// import BubblePosition from './BubblePosition.jsx';
 
 class Chatbox extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
   }
 
-  render () {
+  render() {
     return (
-      <div className='grey chatbox'>
-        {
-          this.props.chatlog.map((value, key) => {
-			  // debugger;
-            return (
-              <ChatBubble 
-                { ...value } //spread operator
-                key={key} 
-                onClick={(e) => this.props.onClick.bind(this, e, value)()}
-              />
-            );
-          })
-        }
+      <div className="grey chatbox">
+        {this.props.chatlog.map((value, key) => {
+          // debugger;
+          return (
+            <ChatBubble
+              {...value} //spread operator
+              key={key}
+              onClick={e => this.props.onClick.bind(this, e, value)()}
+            />
+          );
+        })}
       </div>
     );
   }
 }
 
-
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     chatlog: state.chat.log
   };
 };
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
-    onClick (event, data) {
+    onClick(event, data) {
       if (data.type !== 'button') {
         return;
       }
-      
-      axios.post('/message', data) //redux way of saying once we send a POST request to server, then if we receive a response(Promise) from server
-      .then((response) => {
-        console.log('Response:', response);
 
-        if (response.status === 200) {
-          dispatch({
-            type: 'CHAT_ADD_MESSAGE',
-            payload: {
-              message: response.data,
-              type: 'text',
-              isBot: true,
-            }
-          });
-        }
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
+      axios
+        .post('/message', data) //redux way of saying once we send a POST request to server, then if we receive a response(Promise) from server
+        .then(response => {
+          console.log('Response:', response);
+
+          if (response.status === 200) {
+            dispatch({
+              type: 'CHAT_ADD_MESSAGE',
+              payload: {
+                message: response.data,
+                type: 'text',
+                isBot: true
+              }
+            });
+          }
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
     }
   };
 };
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(Chatbox);
--- client/assets/js/chat/reducers.js
+++ client/assets/js/chat/reducers.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 
-
 module.exports = combineReducers({
   log: require('./reducers/chatlog.js'),
   input: require('./reducers/userInput.js')
