c53db8687e4bad6c6d59028ae1e673c469d161e3
Xi Chen <xch@uw.edu>
2017-08-21 14:46:09 -0700

    removed ChatButton

--- client/assets/js/chat/Chatbar.jsx
+++ client/assets/js/chat/Chatbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import axios from 'axios';
-import ChatButton from './ChatButton.jsx';
+// import ChatButton from './ChatButton.jsx';
 
 
 
