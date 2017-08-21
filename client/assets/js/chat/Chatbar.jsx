947f6c3666a458242d2428ccfb0851e5cc93ef61
Xi Chen <xch@uw.edu>
2017-08-21 14:11:00 -0700

    added send and speak buttons, fixed firefox display error

--- client/assets/js/chat/Chatbar.jsx
+++ client/assets/js/chat/Chatbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import axios from 'axios';
+import ChatButton from './ChatButton.jsx';
 
 
 
@@ -10,20 +11,27 @@ class Chatbar extends React.Component {
     super(props);
   }
 
-  // componentDidMount () {
-    // axios.get('/');
-  // }
 
   render () {
-    return (
+    return (	  
       <div className='light-grey chatbar'>
+	  {/*<ChatButton value='speak' onClick={this.props.onClick.bind(this)}/>*/}
+		<button type='button' value='speak' onClick={this.props.onClick.bind(this)}>
+			speak
+		</button>
+		
         <input 
           type='text'
           placeholder='Type your message here'
+		  ref='textInput'
 		  // onChange={this.props.onChange.bind(this)}
           // onKeyUp={(e) => this.props.onKeyUp.bind(this, e, e.target.value)()}
 		  onKeyUp ={this.props.onKeyUp.bind(this)}
         />
+		{/*<ChatButton value='send' onClick={this.props.onClick.bind(this)}/>*/}
+		 <button type='button' value='send' onClick={this.props.onClick.bind(this)}>
+			send
+		 </button>
       </div>
     );
   }
@@ -48,13 +56,127 @@ const mapDispatchToProps = (dispatch) => {
 			// }
 		// });
 	// },
+	 onClick(event) {
+      //using currentTarget instead of target because currentTarget is the object listening(the button). target is that actual target received, which is the icon, and not what we want
+      if (event.currentTarget.type !== 'button') {
+        return;
+      }
+      console.log('event.currentTarget.value:', event.currentTarget.value);
+      if (event.currentTarget.value === 'speak') {		
+        console.log('speak button clicked');    
+		let recognition, synth, voices;
+		try {
+			const SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+			const SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+			const SpeechRecognitionEvent =
+			  SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+
+			recognition = new SpeechRecognition();
+			recognition.lang = 'en-US';
+				// const result = {};
+			synth = window.speechSynthesis;
+			voices = synth.getVoices();
+		} catch (e){
+			alert('your browser may not support speech recognition');
+			return;
 	
+		} 
+				
+		recognition.start();
+        recognition.onresult = e => {
+          const transcript = e.results[e.results.length - 1][0].transcript;
+          console.log('result: ', transcript);
+          // return result;
+          const data = { message: transcript, type: 'text', isBot: false };
+          dispatch({
+            type: 'CHAT_ADD_MESSAGE',
+            payload: data
+          });
+          dispatch({
+            type: 'USER_INPUT',
+            payload: data
+          });
+
+          axios
+            .post('/message', { payload: data, id: this.props.sessionId })
+            .then(response => {
+              console.log('Response:', response);
+              if (response.status === 200) {
+                dispatch({
+                  type: 'CHAT_ADD_MESSAGE',
+                  payload: {
+                    message: response.data,
+                    type: 'text',
+                    isBot: true
+                  }
+                });
+              }
+              const utt = new SpeechSynthesisUtterance();
+              utt.lang = 'en-US';
+              utt.text = response.data;
+              utt.voice = voices[0];
+              utt.pitch = 5;
+              utt.volume = 5;
+              synth.speak(utt);
+            })
+            .catch(error => {
+              console.error('Error:', error);
+            });
+        };
+
+        recognition.onerror = e => {
+          console.error(e);
+        };
+
+        recognition.onend = () => {
+          console.log('recognition end.');
+        };
+      } else {
+        console.log('send button clicked');
+        console.log('input message', this.refs.textInput.value);
+        const data = {
+          message: this.refs.textInput.value,
+          type: 'text',
+          isBot: false
+        };
+        //clears textbox
+        this.refs.textInput.value = '';
+        console.log('message:', data);
+
+        dispatch({
+          type: 'CHAT_ADD_MESSAGE',
+          payload: data
+        });
+        dispatch({
+          type: 'USER_INPUT',
+          payload: data
+        });
+
+        axios
+          .post('/message', { payload: data, id: this.props.sessionId })
+          .then(response => {
+            console.log('Response:', response);
+            if (response.status === 200) {
+              dispatch({
+                type: 'CHAT_ADD_MESSAGE',
+                payload: {
+                  message: response.data,
+                  type: 'text',
+                  isBot: true
+                }
+              });
+            }
+          })
+          .catch(error => {
+            console.error('Error:', error);
+          });
+      }
+    },
 	onKeyUp (event) {
 		
 		if (event.keyCode === 13){
 			console.log("message:", event.target.value);
 			const data = {message:event.target.value, type:'text', isBot:false};
-			// event.target.value = '';
 			dispatch(
 				{
 				type: 'CHAT_ADD_MESSAGE',
@@ -65,9 +187,8 @@ const mapDispatchToProps = (dispatch) => {
 				type: 'USER_INPUT',
 				payload:data,
 			});
-			// console.log(event.target.value);
 			//clear input bar
-			event.currentTarget.value = '';
+			this.refs.textInput.value = '';
 			
 			axios.post('/message', {payload:data, id:this.props.sessionId})
 			.then((response) =>{
@@ -86,35 +207,10 @@ const mapDispatchToProps = (dispatch) => {
 			.catch((error) => {
 			console.error('Error:', error);
 			});
-		  // console.log(event.target);
 			
 		}
 	}
 	
-    // onKeyDown (event, data) {
-	  // console.log("Message: ", data);
-      // if (event.keyCode === 13) {
-		  // axios.post('/messge', data)
-		  // .then((response) => {
-        // console.log('Response:', response);
-
-        // if (response.status === 200) {
-          // dispatch({
-            // type: 'CHAT_ADD_MESSAGE',
-            // payload: {
-              // message: response.data,
-              // type: 'text',
-              // isBot: true,
-            // }
-          // });
-        // }
-      // })
-	  // .catch((error) => {
-        // console.error('Error:', error);
-      // });
-        
-      // }
-    // }
   };
 };
 
