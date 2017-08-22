bce73f1d50f88a82eec9c1654b5a1252d1d665d7
howdycrystalm <howdycrystalm@gmail.com>
2017-08-22 14:48:37 -0700

    added header. needs positioning

--- client/assets/js/chat/ChatContainer.jsx
+++ client/assets/js/chat/ChatContainer.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 
+import Header from './Header.jsx';
 import Chatbox from './Chatbox.jsx';
 import Chatbar from './Chatbar.jsx';
+
 const uuidv1 = require('uuid/v1');
 const sessionId = uuidv1();
 
-const ChatContainer = () => (
-  <div className='chat-container'>
+const ChatContainer = () =>
+  <div className="chat-container">
     <Chatbox sessionId={sessionId} /> {/*props have to be in {} or ""*/}
     <Chatbar sessionId={sessionId} />
-  </div>
-);
-
+    <Header />
+  </div>;
 
 export default ChatContainer;
