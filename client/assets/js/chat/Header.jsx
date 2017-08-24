38f2fe39a1918ace912b272b691948946282ed1d
howdycrystalm <howdycrystalm@gmail.com>
2017-08-24 10:59:55 -0700

    added icons to header

--- client/assets/js/chat/Header.jsx
+++ client/assets/js/chat/Header.jsx
@@ -5,9 +5,19 @@ class Header extends React.Component {
   render() {
     return (
       <div className="header">
-        <h1>sup!</h1>
         <div>
-          <button type="button">reset</button>
+          <button type="button" className="refresh">
+            <i className="material-icons" style={{ fontSize: '44px' }}>
+              refresh
+            </i>
+          </button>
+        </div>
+        <div>
+          <button type="button" className="settings">
+            <i className="material-icons" style={{ fontSize: '44px' }}>
+              settings
+            </i>
+          </button>
         </div>
       </div>
     );
