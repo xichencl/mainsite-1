a3233b3320f2f7b141a46b65946b02d720fa5213
howdycrystalm <howdycrystalm@gmail.com>
2017-08-24 23:06:24 -0700

    scroll to top works

--- client/assets/js/chat/ChatContainer.jsx
+++ client/assets/js/chat/ChatContainer.jsx
@@ -7,33 +7,36 @@ import Chatbar from './Chatbar.jsx';
 const uuidv1 = require('uuid/v1');
 let sessionId = uuidv1();
 
-class ChatContainer extends React.Component{
-	constructor(props){
-		super(props);
-		this.state={id:sessionId};
-		this.resetSession=this.resetSession.bind(this);
-	}
-	
-	// componentDidMount(){
-		// sessionId = uuidv1();
-	// }
-	
-	// componentWillUnmount(){		
-		
-	// }
-	
-	resetSession(){
-		this.setState({id: uuidv1()});
-	}
-	
-	render (){
-		return(
-		  <div className="chat-container">
-			<Chatbox sessionId={this.state.id} /> {/*props have to be in {} or ""*/}
-			<Chatbar sessionId={this.state.id} />
-			<Header resetSession={this.resetSession}/>
-		  </div>
-		);
-	}
+class ChatContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { id: sessionId };
+    this.resetSession = this.resetSession.bind(this);
+  }
+
+  // componentDidMount(){
+  // sessionId = uuidv1();
+  // }
+
+  // componentWillUnmount(){
+
+  // }
+
+  resetSession() {
+    this.setState({ id: uuidv1() });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="chat-container">
+          <Chatbox sessionId={this.state.id} />{' '}
+          {/*props have to be in {} or ""*/}
+          <Chatbar sessionId={this.state.id} />
+          <Header resetSession={this.resetSession} />
+        </div>
+      </div>
+    );
+  }
 }
 export default ChatContainer;
