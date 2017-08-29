fb1fb531bc21efc08952c1858571de0e395aa693
howdycrystalm <howdycrystalm@gmail.com>
2017-08-29 14:18:52 -0700

    seperated speech bubbles, now each on own line

--- client/assets/js/chat/ChatBubble.jsx
+++ client/assets/js/chat/ChatBubble.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import ReactHtmlParser from 'react-html-parser';
-
 const ChatBubble = props => {
   let className = 'blue bot-bubble-left';
-
   if (props.type === 'button') {
     className = ' button-blue chat-button';
   } else if (props.isBot === false) {
-    className = ' user user-bubble-right';
-    //using '=' instead of '+=' seperates blue chat-bubble from orange user-bubble-right
+    className = ' user user-bubble-right'; //using '=' instead of '+=' seperates blue chat-bubble from orange user-bubble-right
+    return (
+      //allows user bubble and bot bubble to be on two different lines because we're wrapping in div className=bubble-breaker
+      <div className="bubble-breaker">
+        <div className={className} onClick={props.onClick}>
+          {ReactHtmlParser(props.message)}
+        </div>
+      </div>
+    );
   }
   return (
-    <div>
-      <div className={className} onClick={props.onClick}>
-        {ReactHtmlParser(props.message)}
-      </div>
-      {/*<div className="test1" />*/}
+    <div className={className} onClick={props.onClick}>
+      {ReactHtmlParser(props.message)}
     </div>
   );
 };
-
 export default ChatBubble;
