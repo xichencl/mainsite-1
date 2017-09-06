f19c00efa9c2b13f650dba69c304367f415afaca
howdycrystalm <howdycrystalm@gmail.com>
2017-09-06 10:59:49 -0700

    added header code from side-nav, have not added styling. scroll still works. but header not fixed.

--- client/assets/js/chat/Header.jsx
+++ client/assets/js/chat/Header.jsx
@@ -2,51 +2,50 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class Header extends React.Component {
-  constructor(props){
-	  super(props);
+  constructor(props) {
+    super(props);
   }
-  
-  
+
   render() {
     return (
       <div className="header">
         <div>
-          <button type="button" className="refresh" onClick={this.props.onClick.bind(this)}>
+          <button
+            type="button"
+            className="refresh"
+            onClick={this.props.onClick.bind(this)}
+          >
             <i className="material-icons" style={{ fontSize: '44px' }}>
               refresh
             </i>
           </button>
         </div>
         <div>
-          <button type="button" className="settings">
-            <i className="material-icons" style={{ fontSize: '44px' }}>
-              settings
-            </i>
-          </button>
+          <div type="button" className="settings">
+            {/*this div is providing space for the custom settings button from npm package react-burger-menu*/}
+          </div>
         </div>
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     chatlog: state.chat.log
   };
 };
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
-    onClick (event) {
+    onClick(event) {
       if (event.currentTarget.type !== 'button') {
         return;
       }
-	  dispatch({type:"RESET_BOT"}); 
-	  this.props.resetSession();
-      
+      dispatch({ type: 'RESET_BOT' });
+      this.props.resetSession();
     }
   };
 };
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
