2caa7f9198147b8187927930bf535648fc15d5ef
howdycrystalm <howdycrystalm@gmail.com>
2017-09-22 09:13:29 -0700

    tried updating webpack to 3.4.1, still has errors

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,3 +1,189 @@
+// const webpack = require('webpack');
+// const path = require('path');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+//
+// const config = {
+//   context: __dirname,
+//   entry: './src/index.js',
+//   output: {
+//     path: __dirname,
+//     filename: 'bundle.js',
+//   },
+//   module: {
+//     loaders: [{
+//       exclude: /node_modules/,
+//       test: /\.(js|jsx)$/,
+//       loader: 'babel-loader',
+//     },
+//     {
+//       test: /\.scss$/,
+//       loader: ExtractTextPlugin.extract('css-loader!sass-loader'),
+//     },
+//     {
+//       test: /\.(png|jpg|gif)$/,
+//       use: [
+//         {
+//           loader: 'file-loader',
+//           options: {}
+//         }
+//       ]
+//     },
+//     {
+//       test: /\.svg$/,
+//       use: [
+//         {
+//           loader: 'file-loader'
+//         },
+//         {
+//           loader: 'svgo-loader',
+//           options: {
+//             plugins: [
+//               {removeTitle: true},
+//               {convertColors: {shorthex: false}},
+//               {convertPathData: false}
+//             ]
+//           }
+//         }
+//       ]
+//     },],
+//   },
+//   devServer: {
+//     historyApiFallback: true,
+//     contentBase: './',
+//   },
+//   plugins: [
+//     new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } }),
+//     // new webpack.optimize.DedupePlugin(),
+//     new webpack.optimize.OccurrenceOrderPlugin(),
+//     new webpack.optimize.UglifyJsPlugin({
+//       compress: { warnings: false },
+//       output: { comments: false },
+//       mangle: false,
+//       sourcemap: false,
+//       minimize: true,
+//       mangle: { except: ['$super', '$', 'exports', 'require', '$q', '$ocLazyLoad'] },
+//     }),
+//     new ExtractTextPlugin({filename:'src/public/stylesheets/app.css',
+//       allChunks: true,
+//     }),
+//   ],
+// };
+//
+// module.exports = config;
+
+// new webpack.optimize.UglifyJsPlugin({
+//       compress: {
+//         warnings: false,
+//         screw_ie8: true,
+//         conditionals: true,
+//         unused: true,
+//         comparisons: true,
+//         sequences: true,
+//         dead_code: true,
+//         evaluate: true,
+//         if_return: true,
+//         join_vars: true,
+//       },
+//       output: {
+//         comments: false,
+//       },
+//     }),
+
+// //////////
+// const webpack = require('webpack');
+// const path = require('path');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+//
+// const config = {
+//   context: __dirname,
+//   entry: './src/index.js',
+//   output: {
+//     path: __dirname,
+//     filename: 'bundle.js',
+//   },
+//   module: {
+//     loaders: [
+//       {
+//         exclude: /node_modules/,
+//         test: /\.(js|jsx)$/,
+//         loader: 'babel-loader',
+//       },
+//       {
+//         test: /\.scss$/,
+//         loader: ExtractTextPlugin.extract('css-loader!sass-loader'),
+//       },
+//       {
+//         test: /\.(png|jpg|gif)$/,
+//         use: [
+//           {
+//             loader: 'file-loader',
+//             options: {},
+//           },
+//         ],
+//       },
+//       {
+//         test: /\.svg$/,
+//         use: [
+//           {
+//             loader: 'file-loader',
+//           },
+//           {
+//             loader: 'svgo-loader',
+//             options: {
+//               plugins: [
+//                 { removeTitle: true },
+//                 { convertColors: { shorthex: false } },
+//                 { convertPathData: false },
+//               ],
+//             },
+//           },
+//         ],
+//       },
+//     ],
+//   },
+//   devServer: {
+//     historyApiFallback: true,
+//     contentBase: './',
+//   },
+//   plugins: [
+//     new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } }),
+//     new webpack.optimize.DedupePlugin(),
+//     new webpack.optimize.OccurrenceOrderPlugin(),
+//     new webpack.optimize.UglifyJsPlugin({
+//       compress: { warnings: false },
+//       output: { comments: false },
+//       mangle: false,
+//       sourcemap: false,
+//       minimize: true,
+//       mangle: { except: ['$super', '$', 'exports', 'require', '$q', '$ocLazyLoad'] },
+//     }),
+//     new ExtractTextPlugin({
+//       filename: 'src/public/stylesheets/app.css',
+//       allChunks: true,
+//     }),
+//   ],
+// };
+//
+// module.exports = config;
+
+// new webpack.optimize.UglifyJsPlugin({
+//       compress: {
+//         warnings: false,
+//         screw_ie8: true,
+//         conditionals: true,
+//         unused: true,
+//         comparisons: true,
+//         sequences: true,
+//         dead_code: true,
+//         evaluate: true,
+//         if_return: true,
+//         join_vars: true,
+//       },
+//       output: {
+//         comments: false,
+//       },
+//     }),
+// ////////////
 const webpack = require('webpack');
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
@@ -10,42 +196,44 @@ const config = {
     filename: 'bundle.js',
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      test: /\.(js|jsx)$/,
-      loader: 'babel-loader',
-    },
-    {
-      test: /\.scss$/,
-      loader: ExtractTextPlugin.extract('css-loader!sass-loader'),
-    },
-    {
-      test: /\.(png|jpg|gif)$/,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {}  
-        }
-      ]
-    },
-    {
-      test: /\.svg$/,
-      use: [
-        {
-          loader: 'file-loader'
-        },
-        {
-          loader: 'svgo-loader',
-          options: {
-            plugins: [
-              {removeTitle: true},
-              {convertColors: {shorthex: false}},
-              {convertPathData: false}
-            ]
-          }
-        }
-      ]
-    },],
+    loaders: [
+      {
+        exclude: /node_modules/,
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('css-loader!sass-loader'),
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {},
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+          {
+            loader: 'svgo-loader',
+            options: {
+              plugins: [
+                { removeTitle: true },
+                { convertColors: { shorthex: false } },
+                { convertPathData: false },
+              ],
+            },
+          },
+        ],
+      },
+    ],
   },
   devServer: {
     historyApiFallback: true,
@@ -63,7 +251,8 @@ const config = {
       minimize: true,
       mangle: { except: ['$super', '$', 'exports', 'require', '$q', '$ocLazyLoad'] },
     }),
-    new ExtractTextPlugin({filename:'src/public/stylesheets/app.css',
+    new ExtractTextPlugin({
+      filename: 'src/public/stylesheets/app.css',
       allChunks: true,
     }),
   ],
