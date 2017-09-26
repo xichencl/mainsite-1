61a6b9e94e69f6e5d2eb3ab7c4f8c972fa20be20
howdycrystalm <howdycrystalm@gmail.com>
2017-09-26 14:17:48 -0700

    bot slides opens, but needs styling big time

--- client/src/components/chatbot/OpenBot.jsx
+++ client/src/components/chatbot/OpenBot.jsx
@@ -4,48 +4,38 @@ import ReactHtmlParser from 'react-html-parser';
 import ChatIcon from './icons/ChatIcon.jsx';
 import ChatContainer from './ChatContainer.jsx';
 
+const BotBox = ({ visible }) =>
+  <div id="bot" className={visible ? 'slideIn' : 'slideOut'}>
+    <ChatContainer />
+  </div>;
+
+// class NavigationBar extends React.Component = ({ visible }) =>
+//   <div id="navbar" className={visible ? 'slideIn' : 'slideOut'}>
+//     <ChatContainer />
+//   </div>;
+
 class OpenBot extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      // toggle box is closed initially
-      opened: false,
-    };
-    // http://egorsmirnov.me/2015/08/16/react-and-es6-part3.html
-    this.toggleOpen = this.toggleOpen.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = { visible: false };
   }
 
-  toggleOpen() {
-    // check if box is currently opened
-    const { opened } = this.state;
-    this.setState({
-      // toggle value of `opened`
-      opened: !opened,
-    });
+  handleClick() {
+    this.setState(prev => ({ visible: !prev.visible }));
   }
 
   render() {
-    const { title, children } = this.props;
-    const { opened } = this.state;
     return (
-      // <div className="box">
-      <div>
-        <div className="open-bot-icon" onClick={this.toggleOpen}>
-          <ChatIcon />
-        </div>
-        {opened &&
-          <div className="box-content">
-            <ChatContainer />
-          </div>}
+      <div id="wrapper">
+        <button type="button" onClick={this.handleClick}>
+          {this.state.visible ? 'Hide bot' : 'Show bot'}
+        </button>
+        <BotBox visible={this.state.visible} />
       </div>
     );
   }
 }
 
-ReactDOM.render(
-  <OpenBot text="Click me">
-    <div>Some content</div>
-  </OpenBot>,
-  document.getElementById('root'),
-);
+// ReactDOM.render(<OpenBot />, document.getElementById('root'));
 export default OpenBot;
