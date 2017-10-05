fd7564000f3257bd29e873d60209915de67cfb98
taypsl <tayparsell@gmail.com>
2017-10-05 15:39:16 -0700

    updating frontend of case components

--- client/src/components/cases/my-case-data.js
+++ client/src/components/cases/my-case-data.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 
 class MyCaseData extends Component {
-  componentWillMount() {
+  componentDidMount() {
     // Fetch user data prior to component mounting
   }
 
