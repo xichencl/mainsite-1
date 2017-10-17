7eec045b0c97c0700fe7940c8a144f5ff90955d1
howdycrystalm <howdycrystalm@gmail.com>
2017-10-17 10:25:13 -0700

    put header icons in container

--- client/src/components/chatbot/Header.jsx
+++ client/src/components/chatbot/Header.jsx
@@ -1,5 +1,54 @@
+// import React from 'react';
+// import { connect } from 'react-redux';
+// import CloseChat from './icons/CloseChat.jsx';
+//
+// class Header extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//
+//   render() {
+//     return (
+//       <div className="header-L">
+//         <div>
+//           {/* replaced button element with div for styling purposes*/}
+//           <button type="button" className="refresh-M" onClick={this.props.onClick.bind(this)}>
+//             <i className="material-icons" style={{ fontSize: '44px' }}>
+//               refresh
+//             </i>
+//           </button>
+//           <CloseChat />
+//         </div>
+//         <div>
+//           {/* settings icon*/}
+//           <div className="settings-N">
+//             {/* this div is providing space for the custom settings button from npm package react-burger-menu*/}
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+//
+// const mapStateToProps = state => ({
+//   chatlog: state.chat.log,
+// });
+//
+// const mapDispatchToProps = dispatch => ({
+//   onClick(event) {
+//     if (event.currentTarget.type !== 'button') {
+//       return;
+//     }
+//     dispatch({ type: 'RESET_BOT' });
+//     this.props.resetSession();
+//   },
+// });
+//
+// export default connect(mapStateToProps, mapDispatchToProps)(Header);
+
 import React from 'react';
 import { connect } from 'react-redux';
+import CloseChat from './icons/CloseChat.jsx';
 
 class Header extends React.Component {
   constructor(props) {
@@ -9,19 +58,13 @@ class Header extends React.Component {
   render() {
     return (
       <div className="header-L">
-        <div>
-          {/* replaced button element with div for styling purposes*/}
+        <div className="icon-container">
+          <CloseChat />
           <button type="button" className="refresh-M" onClick={this.props.onClick.bind(this)}>
             <i className="material-icons" style={{ fontSize: '44px' }}>
               refresh
             </i>
           </button>
-        </div>
-        <div>
-          {/* settings icon*/}
-          <div className="settings-N">
-            {/* this div is providing space for the custom settings button from npm package react-burger-menu*/}
-          </div>
         </div>
       </div>
     );
