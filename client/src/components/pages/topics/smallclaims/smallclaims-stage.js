480158db0e5a396239296245d54533523028e077
Taylor Parsell <tayparsell@gmail.com>
2018-02-20 04:27:34 -0800

    working on small claims accordion box content feature

--- client/src/components/pages/topics/smallclaims/smallclaims-stage.js
+++ client/src/components/pages/topics/smallclaims/smallclaims-stage.js
@@ -6,22 +6,8 @@ import { fetchStages } from '../../../../actions/content.js';
 import TextIconBox from '../../../template/text-icon-box';
 import ChecklistIcon from '../../../../img/icn_checklist.svg';
 import InfoBox from '../../../template/info-box';
+import AccordionBoxContainer from '../../../template/accordion-box/accordion-box-container';
 
-const resourceList = [
-  { 
-    title: "Small Claims Advisor",
-    link: "http://www.courts.ca.gov/selfhelp-advisors.htm"
-  },
-  { 
-    title: "Non-profit Agencies",
-    link: "http://www.courts.ca.gov/selfhelp-lowcosthelp.htm#Legal_aid_agencies_and_other_non-profit_groups"
-  },
-  { 
-    title: "Small Claims Advisor",
-    link: "http://www.courts.ca.gov/selfhelp-advisors.htm"
-  },
-]
-    
 
 class SmallClaimsStage extends Component {
   constructor(props) {
@@ -48,13 +34,7 @@ class SmallClaimsStage extends Component {
   }
 
   render() {
-    const resources = resourceList.map((item) => {
-      return (
-        <div>
-          <a href={item.link} target="_blank">{item.title}</a>
-        </div>
-      )
-    })
+
     return (
       <div>
         <div className="mainpage-title">
@@ -62,20 +42,7 @@ class SmallClaimsStage extends Component {
           <h1>Small Claims</h1>
           <hr className="mainpage-title-line"/>
         </div>
-        <div className="grid grid-pad">
-          <TextIconBox 
-            boxTitle="Small Claims Checklist"
-            boxContent="Use our interactive checklist to help you manage your small claims case before you file, during your case, and after a judgement has been made."
-            iconLarge={ChecklistIcon}
-            TextIconBoxClass="Box Text-icon-box Grey-background medium-box"
-            buttonLink="/test-todo"
-            />
-          <InfoBox 
-            boxTitle="Resources"
-            boxContent={resources}
-            buttonVisibilityClass="hidden"
-            infoboxClass="Box Info-box small-box col-2"
-            />
+
           
           {/*<div className="White-background">{this.props.match.params.stage} Your Case</div>
           <div className="col-2 Grey-background">menu*/}
@@ -84,6 +51,14 @@ class SmallClaimsStage extends Component {
             <Link to={`${this.props.match.url}/during`}>during</Link>
             <Link to={`${this.props.match.url}/after`}>after</Link>*/}
           <div className="grid grid-pad">
+            <AccordionBoxContainer />
+            <InfoBox 
+              boxTitle={`Menu - ${this.state.stageTitle}`}
+              boxContent={renderedLinks}
+              buttonVisibilityClass="hidden"
+              infoboxClass="Box Info-box small-box col-2"
+            />
+          
             {renderStages()}  
           </div>
 
