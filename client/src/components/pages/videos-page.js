b4eb31e25f76353741c58cebe1e40b0cd7657ab1
Michael Daly <Mdaly@contracosta.courts.ca.gov>
2018-03-12 14:27:06 -0700

    Very temporary and WIP "Video Resources" page

--- client/src/components/pages/videos-page.js
+++ client/src/components/pages/videos-page.js
@@ -1,10 +1,46 @@
 import React, {Component} from 'react';
-import TitleLine from '../template/title-line';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-export default class Videos extends Component {
+import TitleLine from '../template/title-line';
+import { fetchVideos } from '../../actions/content.js';
+
+
+class Videos extends Component {
+  componentWillMount() {
+    if (!this.props.videos) {
+      this.props.fetchVideos()
+      console.log(this.props.videos, 'this.props.videos')
+    }
+  }
+
+  renderVideos() {
+    return this.props.videos.map((unit, index) => {
+      return (
+        <div className="Square-box-container" key={unit.sys.id}>
+          <Link to={'videos/' + unit.fields.title}>
+            <h1>{unit.fields.title}</h1>
+          </Link>
+        </div>
+      );
+    });
+  }
 	render() {
 		return (
-			<div><TitleLine title="Video Resources" /></div>
+			<div>
+        <TitleLine title="Video Resources" />
+        <div className="grid grid-pad">
+          {this.renderVideos()}
+        </div>
+      </div>
 		)
 	}
 }
+
+function mapStateToProps(state) {
+  return { videos: state.content.videos,
+           assets: state.content.assets
+   };
+}
+
+export default connect(mapStateToProps, { fetchVideos })(Videos);
