50fe27b7a723dbba01064f8a59f6ea25067d1a86
Xi Chen <xch@uw.edu>
2018-03-21 11:43:05 -0700

    search results working, need to link up with redux state

--- client/src/components/pages/search-results-page.js
+++ client/src/components/pages/search-results-page.js
@@ -1,94 +1,53 @@
 import React from 'react';
+import {withRouter} from 'react-router';
+
 
 class SearchResults extends React.Component{
+	constructor(props){
+		super(props);
+		this.gcseCallback = this.gcseCallback.bind(this);
+	}
 
-	ComponentWillMount(){
-            var config = {
-          gcseId: '010012988809159316594:zgkefy-h6yk',
-          resultsUrl: 'http://localhost:8000/search-results',
-          searchWrapperClass: 'gcse-search-wrapper',
-          resultsWrapperClass: 'gcse-results-wrapper'
-        };
 
-        var renderSearchForms = function () {
-          if (document.readyState == 'complete') {
-            queryAndRender();
-          } else {
-            google.setOnLoadCallback(function () {
-              queryAndRender();
-            }, true);
-          }
-        };
+	gcseCallback() {
+	  if (document.readyState != 'complete')
+	    return google.setOnLoadCallback(this.gcseCallback, true);
+	  google.search.cse.element.render({gname:'gsearch', div:'testResults', tag:'searchresults-only'});
+	  const element = google.search.cse.element.getElement('gsearch');
+	  console.log("location", this.props.location);
+	  console.log("history", this.props.history);
+	  element.execute(this.props.location.state.query);
+	};
 
-        var queryAndRender = function() {
-          var gsceSearchForms = document.querySelectorAll('.' + config.searchWrapperClass);
-          var gsceResults = document.querySelectorAll('.' + config.resultsWrapperClass);
+	componentDidMount(){
+    
+		(function() {
+		  var cx = '010012988809159316594:zgkefy-h6yk';
+		  var gcse = document.createElement('script');
+		  gcse.type = 'text/javascript';
+		  gcse.async = true;
+		  gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+		    '//cse.google.com/cse.js?cx=' + cx;
+		  var s = document.getElementsByTagName('script')[0];
+		  s.parentNode.insertBefore(gcse, s);
+		})();
 
-          if (gsceSearchForms) {
-            console.log("found search forms", gsceSearchForms);
-            renderSearch(gsceSearchForms[0]);
-          }
-          if (gsceResults) {
-            renderResults(gsceResults[0]);
-          }
-        };
-
-        var renderSearch = function (div) {
-
-            google.search.cse.element.render(
-              {
-                div: div.id,
-                tag: 'searchbox-only',
-                attributes: {
-                  resultsUrl: config.resultsUrl
-                }
-              }
-            );
-
-            console.log("google element has been created");
-            if (div.dataset &&
-                div.dataset.stylingFunction &&
-                window[div.dataset.stylingFunction] &&
-                typeof window[div.dataset.stylingFunction] === 'function') {
-              window[div.dataset.stylingFunction](form);
-            }
-        };
-
-        var renderResults = function(div) {
-          google.search.cse.element.render(
-            {
-              div: div.id,
-              tag: 'searchresults-only'
-            });
-        };
-
-        window.__gcse = {
-          parsetags: 'explicit',
-          callback: renderSearchForms
-        };
-
-        (function () {
-          var cx = config.gcseId;
-          var gcse = document.createElement('script');
-          gcse.type = 'text/javascript';
-          gcse.async = true;
-          gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-            '//cse.google.com/cse.js?cx=' + cx;
-          var s = document.getElementsByTagName('script')[0];
-          s.parentNode.insertBefore(gcse, s);
-        })();
-
-  }
+		window.__gcse = {
+		  parsetags: 'explicit',
+		  callback: this.gcseCallback
+		};
+  	}
 
   render(){
   	return (
     <div>
-  	{/*<div className="gcse-search-wrapper" id="testSearch"></div>*/}
-  	{<div className="gcse-results-wrapper" id="testResults"></div>}
+  	<span>Search Results</span>
+  	<div className="gcse-results-wrapper" id="testResults"></div>
   	</div>
   	);
   }
 
 } 
+
 
 export default SearchResults;	
--- client/src/components/template/search.js
+++ client/src/components/template/search.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Redirect, withRouter } from 'react-router';
-
+import axios from 'axios';
+import SearchResults from '../pages/search-results-page';
+import { connect } from 'react-redux'; 
 
 class Search extends Component {
   constructor(props){
@@ -18,91 +20,99 @@ class Search extends Component {
   handleSubmit(event){
     //process query string
     console.log("form submitted");
-    let queryStr = this.state.value.trim().toLowerCase();
-    queryStr = '/?q='+encodeURIComponent(queryStr);
-    console.log("query string: ", queryStr);
-    event.preventDefault();
-    console.log("props", this.props);
-    this.props.history.push('/search-results'+queryStr);
+    // let queryStr = this.state.value.trim().toLowerCase();
+    // queryStr = '?q='+encodeURIComponent(queryStr);
+    // console.log("query string: ", queryStr);
+    // event.preventDefault();
+    // console.log("props", this.props);
+    this.props.history.push('/search-results', {query: this.state.value});
+    // axios.get('/api/results/'+queryStr)
+    // .then((response)=> {console.log('redirected')})
+    // .catch((error) => {console.log(error)});
+      
   }
 
-  componentDidMount(){
-            var config = {
-          gcseId: '010012988809159316594:zgkefy-h6yk',
-          resultsUrl: 'http://localhost:8000/search-results',
-          searchWrapperClass: 'gcse-search-wrapper',
-          resultsWrapperClass: 'gcse-results-wrapper'
-        };
 
-        var renderSearchForms = function () {
-          if (document.readyState == 'complete') {
-            queryAndRender();
-          } else {
-            google.setOnLoadCallback(function () {
-              queryAndRender();
-            }, true);
-          }
-        };
+  
+  // componentDidMount(){
+  //           var config = {
+  //         gcseId: '010012988809159316594:zgkefy-h6yk',
+  //         resultsUrl: 'http://localhost:8000/search-results',
+  //         searchWrapperClass: 'gcse-search-wrapper',
+  //         resultsWrapperClass: 'gcse-results-wrapper'
+  //       };
 
-        var queryAndRender = function() {
-          var gsceSearchForms = document.querySelectorAll('.' + config.searchWrapperClass);
-          var gsceResults = document.querySelectorAll('.' + config.resultsWrapperClass);
+  //       var renderSearchForms = function () {
+  //         if (document.readyState == 'complete') {
+  //           queryAndRender();
+  //         } else {
+  //           google.setOnLoadCallback(function () {
+  //             queryAndRender();
+  //           }, true);
+  //         }
+  //       };
 
-          if (gsceSearchForms) {
-            console.log("found search forms", gsceSearchForms);
-            renderSearch(gsceSearchForms[0]);
-          }
-          if (gsceResults) {
-            renderResults(gsceResults[0]);
-          }
-        };
+  //       var queryAndRender = function() {
+  //         var gsceSearchForms = document.querySelectorAll('.' + config.searchWrapperClass);
+  //         var gsceResults = document.querySelectorAll('.' + config.resultsWrapperClass);
 
-        var renderSearch = function (div) {
+  //         if (gsceSearchForms) {
+  //           console.log("found search forms", gsceSearchForms);
+  //           renderSearch(gsceSearchForms[0]);
+  //         }
+  //         if (gsceResults) {
+  //           console.log("found results forms", gsceResults);
+  //           renderResults(gsceResults[0], );
+  //         }
+  //       };
 
-            google.search.cse.element.render(
-              {
-                div: div.id,
-                tag: 'searchbox-only',
-                attributes: {
-                  resultsUrl: config.resultsUrl
-                }
-              }
-            );
+  //       var renderSearch = function (div) {
 
-            console.log("google element has been created");
-            if (div.dataset &&
-                div.dataset.stylingFunction &&
-                window[div.dataset.stylingFunction] &&
-                typeof window[div.dataset.stylingFunction] === 'function') {
-              window[div.dataset.stylingFunction](form);
-            }
-        };
+  //           google.search.cse.element.render(
+  //             {
+  //               div: div.id,
+  //               tag: 'searchbox-only',
+  //               attributes: {
+  //                 resultsUrl: config.resultsUrl
+  //               }
+  //             }
+  //           );
 
-        var renderResults = function(div) {
-          google.search.cse.element.render(
-            {
-              div: div.id,
-              tag: 'searchresults-only'
-            });
-        };
+  //           console.log("google element has been created");
+  //           if (div.dataset &&
+  //               div.dataset.stylingFunction &&
+  //               window[div.dataset.stylingFunction] &&
+  //               typeof window[div.dataset.stylingFunction] === 'function') {
+  //             window[div.dataset.stylingFunction](form);
+  //           }
+  //       };
 
-        window.__gcse = {
-          parsetags: 'explicit',
-          callback: renderSearchForms
-        };
+  //       var renderResults = function(div) {
+  //         console.log("renderResults div: ", div);
+  //         google.search.cse.element.render(
+  //           {
+  //             div: div.id,
+  //             tag: 'searchresults-only'
+  //           });
+  //       };
 
-        (function () {
-          var cx = config.gcseId;
-          var gcse = document.createElement('script');
-          gcse.type = 'text/javascript';
-          gcse.async = true;
-          gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-            '//cse.google.com/cse.js?cx=' + cx;
-          var s = document.getElementsByTagName('script')[0];
-          s.parentNode.insertBefore(gcse, s);
-        })();
+  //       window.__gcse = {
+  //         parsetags: 'explicit',
+  //         callback: renderSearchForms
+  //       };
 
-  }
+  //       (function () {
+  //         var cx = config.gcseId;
+  //         var gcse = document.createElement('script');
+  //         gcse.type = 'text/javascript';
+  //         gcse.async = true;
+  //         gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+  //           '//cse.google.com/cse.js?cx=' + cx;
+  //         var s = document.getElementsByTagName('script')[0];
+  //         s.parentNode.insertBefore(gcse, s);
+  //       })();
+
+  // }
 
   
 
@@ -115,16 +125,25 @@ class Search extends Component {
             <form onSubmit={this.handleSubmit} value="search">         
              <div className="Search">
                 <input className="Search-bar" type="text" placeholder=" Search our site" onChange={this.handleChange} value={this.state.value} />
-                
+                <input value="search" type="submit"></input>
              </div>
              </form>
           
-          <div className="gcse-search-wrapper" id="testSearch"></div>
-          {/*<div className="gcse-results-wrapper" id="testResults"></div>*/}
+          {/*<div className="gcse-search-wrapper" id="testSearch"></div>
+          <div className="gcse-results-wrapper" id="testResults"></div>*/}
         </div>
         
     );
   }
 }
 
+// const mapDispatchToProps = (dispatch) => ({
+//     handleSubmit: (e) => {
+//       e.preventDefault();
+//       dispatch({type: 'STORE_QUERY', action: {query: this.state.query}})
+//       this.props.history.push('/search-results');
+//     }
+// });
+
+// export default connect(null, mapDispatchToProps)(Search);
 export default withRouter(Search);
